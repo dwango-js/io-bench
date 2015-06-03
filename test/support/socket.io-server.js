1e479255b246958aae2b05c2dc11b5bc587fb5f2
Naoyuki-Kanezawa <naoyuki_kanezawa@dwango.co.jp>
2015-06-03 22:22:24 +0900

    dont use native ack on socket.io

--- test/support/socket.io-server.js
+++ test/support/socket.io-server.js
@@ -2,11 +2,7 @@ var io = require('socket.io');
 var server = io(process.env.PORT || 3000);
 
 server.on('connection', function(socket) {
-  socket.on('message', function(data, callback) {
-    if (callback) {
-      callback(data);
-    } else {
-      socket.send(data);
-    }
+  socket.on('message', function(data) {
+    socket.send(data);
   });
 });
